374f531d56db0b441995e97e5d8a3174487dbf9a
Mariia Kolo <marikolo57@gmail.com>
2026-02-21 12:06:05 +0000

    Added method Stubs

--- src/griffith/Conversions.java
+++ src/griffith/Conversions.java
@@ -4,6 +4,23 @@
 package griffith;
 
 public class Conversions {
+    public double  euroToDollar(double euro){
+    return 0;
+    }
 
+    public double dollarToEuro(double dollar){
+    return 0;
+    }
 
+    public int stringToInteger (String val){
+    return 0;
+    }
+
+    public String integerToString (int val){
+    return null;
+    }
+    // change uppercase to lowercase and vice versa
+    public String switchCase(){
+        return null;
+    }
 }
