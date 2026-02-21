5e0901e7eee9d05d2999ef32fc13a50156a46049
Mariia Kolo <marikolo57@gmail.com>
2026-02-21 12:20:30 +0000

    Wrote unit test for euroToDollar, test failed

--- src/griffith/ConversionsTest.java
+++ src/griffith/ConversionsTest.java
@@ -3,8 +3,35 @@
 
 package griffith;
 
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class ConversionsTest {
+
+    Conversions conv = new Conversions();
+
+    @Test
+    void testEuroToDollar(){
+        assertEquals(10.0, conv.euroToDollar(9), 0.01);
+        assertEquals(0.0, conv.euroToDollar(0), 0.01);
+        assertEquals(-10.0, conv.euroToDollar(-9), 0.01);
+    }
+
+    void testDollarToEuro(){
+
+    }
+
+    void testStringToInteger (){
+
+    }
+
+   void testIntegerToString (){
+
+    }
+
+    void terstSwitchCase(){
+
+    }
 
 }
